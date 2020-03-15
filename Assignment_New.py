35c388494a5da1c8ff894073affec861b434d3e5
mchatterjee4697 <maharshi4697@gmail.com>
2020-03-15 21:47:26 +0530

    Final Commit

--- Assignment_New.py
+++ Assignment_New.py
@@ -1,7 +1,6 @@
 # Importing all the required libraries.
 import json
 from collections import OrderedDict
-import re
 import click
 import pandas as pd
 from jsonschema import validate
@@ -38,83 +37,12 @@ def transformations(df, schema):
     df_out = df.to_dict(into=OrderedDict, orient='index')
     return df_out
 
-
-# Running Regex Commands for Data Validation.
-# Better alternative is using Pattern in the JSON Schema
-# Demonstration done in Assignment_New.py invoking Schema_New.json
-def data_validation(df):
-
-    # Assigning Regex patterns for all the columns to validate.
-    person_id_pattern = re.compile(r'^[0-9]+$')
-    date_pattern = re.compile(r'^([1-9]|[1][0-2])[/]([1-9]|[1,2]\d|[3][0,1])[/](\d\d)\s([1]{0,1}\d|[2][0-3])[:]([0-5]\d)+$')
-    floor_access_pattern = re.compile(r'^\d{0,1}\d{1}$')
-    building_pattern = re.compile(r'^[A-C]$')
-
-    # Errors are going to be stored in these 2 lists, will later be converted to Ordered Dict.
-    error_row = []
-    error_column = []
-
-    # Correct columns are going to stored in these 4 lists, will later be converted to Ordered Dict.
-    col1 = []
-    col2 = []
-    col3 = []
-    col4 = []
-
-    # Accessing all the data.
-    for i in df:
-
-        # If person_id doesn't match, then store the error and the column value.
-        if(re.search(person_id_pattern, list(df[i].values())[0])) is None:
-            error_row.append(df[i])
-            error_column.append(list(df[i].keys())[0])
-
-        # If date_pattern doesn't match, then store the error and the column value.
-        elif(re.search(date_pattern, list(df[i].values())[1])) is None:
-            error_row.append(df[i])
-            error_column.append(list(df[i].keys())[1])
-
-        # If floor_access_pattern doesn't match, then store the error and the column value.
-        elif(re.search(floor_access_pattern, str(list(df[i].values())[2]))) is None:
-            error_row.append(json.dumps(df[i]))
-            error_column.append(list(df[i].keys())[2])
-
-        # If building_pattern doesn't match, then store the error and the column value.
-        elif(re.search(building_pattern, list(df[i].values())[3])) is None:
-            error_row.append(json.dumps(df[i]))
-            error_column.append(list(df[i].keys())[3])
-
-        # Store all the correct values in lists.
-        else:
-            col1.append(list(df[i].values())[0])
-            col2.append(list(df[i].values())[1])
-            col3.append(list(df[i].values())[2])
-            col4.append(list(df[i].values())[3])
-
-    # Correct Lists are first converted to DataFrame and then finally OrderedDict
-    correct_df = pd.DataFrame({list(df[0].keys())[0]:col1,
-                               list(df[1].keys())[1]:col2,
-                               list(df[2].keys())[2]:col3,
-                               list(df[3].keys())[3]:col4})
-    correct = correct_df.to_dict(into=OrderedDict, orient='index')
-
-    # Error Lists are first converted to DataFrame.
-    error_df = pd.DataFrame({'error_row':error_row,
-                             'error_column':error_column})
-
-    # If Errors exist,then are converted to OrderedDict and returned.
-    # Else None is returned along with the correct list.
-    if error_df.empty is False:
-        errors = error_df.to_dict(into=OrderedDict, orient='index')
-        return correct, errors
-    else:
-        return correct, None
-
 # Validation of the input against the schema.
 def schema_validation(df, schema):
 
     # Errors are going to be stored in these 2 lists, will later be converted to Ordered Dict.
     error_row = []
-    error_column = []
+    error_message = []
 
     # Correct columns are going to stored in these 4 lists, will later be converted to Ordered Dict.
     col1 = []
@@ -136,9 +64,9 @@ def schema_validation(df, schema):
         except Exception as e:
             # The errors rows and their corresponding messages are appended in the following list
             error_row.append(df[i])
-            error_column.append(json.dumps(e.message))
+            error_message.append(json.dumps(e.message))
 
-    # Correct Lists are first converted to DataFrame and then finally OrderedDict.
+    # Correct Lists are first converted to DataFrame and then finally OrderedDict
     correct_df = pd.DataFrame({list(df[0].keys())[0]:col1,
                                list(df[1].keys())[1]:col2,
                                list(df[2].keys())[2]:col3,
@@ -147,7 +75,7 @@ def schema_validation(df, schema):
 
     # Error Lists are first converted to DataFrame.
     error_df = pd.DataFrame({'error_row':error_row,
-                             'error_column':error_column})
+                             'error_message':error_message})
 
     # If Errors exist,then are converted to OrderedDict and returned.
     # Else None is returned along with the correct list.
@@ -190,26 +118,14 @@ def run(csv_path, schema_path, output_path, errors_path):
     # Transform the input csv based on json schema.
     df_transform = transformations(input_df, json_schema)
 
-    # Perform data validation for the datausing regex.
-    correct_data, errors_data = data_validation(df_transform)
 
-    # Perform schema validation for the data using json schema.
-    correct_schema, errors_schema = schema_validation(correct_data, json_schema)
-
-    #Append Errors of Data Validation and Schema Validation Together
-    if errors_data is not None and errors_schema is not None:
-        errors_data.update(errors_schema)
-        errors = errors_data
-    elif errors_data is None and errors_schema is not None:
-        errors = errors_schema
-    elif errors_schema is None and errors_schema is None:
-        errors = errors_data
+    # Perform schema validation for the data.
+    correct_schema, errors_schema = schema_validation(df_transform, json_schema)
 
     # Output jsons.
     output_json(correct_schema, json_output_path)
-    if errors is not None:
-        output_json(errors, errors_output_path)
+    if errors_schema is not None:
+        output_json(errors_schema, errors_output_path)
 
-#Main Function
 if __name__ == "__main__":
     run()
--- README.md
+++ README.md
@@ -18,3 +18,16 @@ To Run the following code, the following steps need to performed.
 
 5. After the previous command is run, enter the details as prompted in the terminal.Upon confusion, the following command can be run.
     python Assignment.py --help
+
+
+
+The Python Script, 'Assignment_New.py' is a more optimized form of 'Assignment.py'. To run this code, use the Json schema, 'schema_new.json' instead of 'schema.json'. The schema_new.json has been modified to verify patterns using the schema instead of hard coding it. The benefit is that only 1 source of truth needs to be maintained and everytime something needs to be changed, the code doesn't need to be changed, only the 'schema_new.json' should be modified.
+
+
+
+To run the python script 'Assignment_New.py' repeat the above steps (1-3).
+
+4. Run the python file using the following command.
+		python Assignment.py
+
+5. In the prompt, enter json path of 'schema_new.json'. In order to generate errors.json, use the 'data_new.csv' instead of 'data.csv'
